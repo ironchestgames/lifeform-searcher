d52475167113626b3125f656272053e012f3032c
Fredrik Järnbröst <contact@fredrikvestin.com>
2018-08-22 21:51:49 +0200

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Stage } from "react-pixi-fiber";
+import SimpleRect from "./SimpleRect"
+
+// PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Stage width={800} height={600} options={{ backgroundColor: 0x10bb99 }}>
+        <SimpleRect />
+      </Stage>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/SimpleRect.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Texture } from 'pixi.js'
+import { Sprite } from "react-pixi-fiber";
+
+class SimpleRect extends Component {
+	state = {
+		width: 100,
+		tint: 0x000000,
+	}
+
+	render() {
+		return (
+			<Sprite
+				texture={Texture.WHITE}
+				x={100}
+				y={100}
+				width={this.state.width}
+				height={100}
+				tint={this.state.tint}
+				interactive={true}
+				pointertap={() => {
+					this.setState(prevState => ({
+						width: prevState.width + 50,
+						tint: prevState.tint + 0x151515,
+					}))
+				}}
+				/>
+		)
+	}
+}
+
+export default SimpleRect
