c7b12d278b7e846fc8c17b48d893a4d9480e96f9
Fredrik Järnbröst <contact@fredrikvestin.com>
2018-09-01 14:30:20 +0200

    added console logged version

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+import version from './version'
+
+console.log('Lifeform Searcher ' + version)
+
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
