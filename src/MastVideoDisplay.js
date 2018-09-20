9c1156cb11e04b2b218268a005bc985941383031
Fredrik Järnbröst <contact@fredrikvestin.com>
2018-09-20 10:00:02 +0200

    added masking to MastVideoDisplay CRTFilter rendering

--- src/MastVideoDisplay.js
+++ src/MastVideoDisplay.js
@@ -10,7 +10,7 @@ import videoFrameImg from './assets/images/video_display_frame.png'
 const WIDTH = 58
 const HEIGHT = 46
 
-const crtFilter = new CRTFilter({
+const crtFilters = [new CRTFilter({
 	curvature: 0,
 	lineWidth: 0,
 	lineContrast: 0,
@@ -19,36 +19,56 @@ const crtFilter = new CRTFilter({
 	vignetting: 0.33,
 	vignettingAlpha: 0.7,
 	vignettingBlur: 0.3,
-})
+})]
 let seedTime = 0
 
 class MastVideoDisplay extends Component {
 
+	constructor(props) {
+		super(props)
+		this.textureVideoFrame = PIXI.Texture.from(videoFrameImg)
+
+		this.videoFrameMask = new PIXI.Graphics()
+		this.videoFrameMask.beginFill()
+		this.videoFrameMask.drawRect(props.x, props.y, WIDTH, HEIGHT)
+		this.videoFrameMask.endFill()
+
+		this.saveLifeform = this.saveLifeform.bind(this)
+	}
+
 	componentDidMount() {
 		this.context.app.ticker.add((dt) => {
-			crtFilter.time += 0.4 * dt
+			crtFilters[0].time += 0.4 * dt
 
 			seedTime += 0.3 * dt
 			if (seedTime > 1) {
-				crtFilter.seed = Math.random()
+				crtFilters[0].seed = Math.random()
 				seedTime = 0
 			}
 		})
 	}
 
+	saveLifeform() {
+		// TODO: save the lifeform frequency
+	}
+
 	render() {
 		return (
 			<Container x={this.props.x || 0} y={this.props.y || 0} >
-				<Sprite
-					texture={PIXI.Texture.WHITE /* TODO: show lifeforms here */}
+				<Container
 					x={1}
 					y={1}
-					width={WIDTH - 2}
-					height={HEIGHT - 2}
-					filters={[crtFilter]}
-					/>
+					mask={this.videoFrameMask}
+					>
+					<Sprite
+						texture={PIXI.Texture.WHITE /* TODO: show lifeforms here */}
+						width={WIDTH - 2}
+						height={HEIGHT - 2}
+						filters={crtFilters}
+						/>
+					</Container>
 				<Sprite
-					texture={PIXI.Texture.from(videoFrameImg)}
+					texture={this.textureVideoFrame}
 					x={-2}
 					y={-2}
 					/>
@@ -60,9 +80,7 @@ class MastVideoDisplay extends Component {
 					text={'sAve'}
 					textOffset={[2, 2]}
 					state={Button.ENABLED}
-					pointertap={(event) => {
-						// TODO: save lifeform on frequency
-					}}
+					pointertap={this.saveLifeform}
 					/>
 			</Container>
 		)
