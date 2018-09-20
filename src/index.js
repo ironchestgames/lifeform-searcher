50f7de12a9875ef92d45c65b4c3a31d40583f31d
Fredrik Järnbröst <contact@fredrikvestin.com>
2018-09-20 19:02:17 +0200

    added react-perf-devtool

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { registerObserver } from 'react-perf-devtool'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 import version from './version'
 
 console.log('Lifeform Searcher ' + version)
+
+// assign the observer to the global scope, as the GC will delete it otherwise
+window.observer = registerObserver()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
